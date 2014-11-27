d81fb4f23f0108b16183301dd9c260d66a3c4a93
CarinaSemerdzhan <sem.carina@gmail.com>
2014-11-27 12:27:15 +0100

    Module

--- cleverPopup.js
+++ cleverPopup.js
@@ -1,91 +1,94 @@
-$(function() {
-  var cleverPopup = function() {
+var Module = (function() {
 
-    this.condition = function(callback){
+    // locally scoped Object
+    obj = {}
+
+    obj.condition = function(callback){
         var result = false;
             area = (function() {
-                $(window).on("mousemove", function(e){
+                window.addEventListener("mousemove", function(e){
                     if (e.pageY <= window.pageYOffset && e.pageY >= 0) {
                         result = true;
+                        console.log('area');
                         callback(result)
                     }
                });
             })();
-            //jquery plugin activities
-            activities = (function() {
-                $('body').activity({
-            	    'achieveTime':60
-                    ,'testPeriod':10
-                    ,useMultiMode: 1
-                    ,callBack: function (e) {
-            	        result = true;
-                        callback(result);
-            	    }
-            	});
-            })();
-            timer =  (function() {
+
+            timer =  function() {
                 var date = new Date();
                     startDate = date.getTime();
-                    finishDate = startDate+120000;
+                    finishDate = startDate+10000;
                     
                     setInterval (function(){
                         var countDate = new Date();
                         if (countDate.getTime() >= finishDate) {
                             result = true;
+                            console.log('timer');
                             callback(result) 
-                        }	       
+                        }          
                    },1000); 
-            })();
+            };
+    }
+    
+    obj.cookies = {
+        getCookie : function(name){
+              var matches = document.cookie.match(new RegExp(
+                "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+              ));
+              return matches ? decodeURIComponent(matches[1]) : undefined;
         },
-    
-    this.cookies = {
-            createCookie: function (name, value, days) {
-                var expires;
-                if (days) {
-                    var date = new Date();
-                    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-                    expires = "; expires=" + date.toGMTString();
-                }
-                else {
-                    expires = "";
-                }
-                document.cookie = name + "=" + value + expires + "; path=/";
-            },
-            getCookie: function (c_name) {
-                if (document.cookie.length > 0) {
-                c_start = document.cookie.indexOf(c_name + "=");
-                    if (c_start != -1) {
-                        c_start = c_start + c_name.length + 1;
-                        c_end = document.cookie.indexOf(";", c_start);
-                        if (c_end == -1) {
-                            c_end = this.createCookie(name, value, days);
-                        }
-                        return unescape(document.cookie.substring(c_start, c_end));
-                    }
-                }
-                return "";  
-            },
-            eraseCookie: function(name) {
-                this.createCookie(name,"",-1)
+        setCookie : function(name, value, options){
+            options = options || {};
+
+            var expires = options.expires;
+            
+            if (typeof expires === 'number') {
+                var days = expires, t = expires = new Date();
+                    t.setDate(t.getDate() + days);
             }
+            
+            
+            if (expires && expires.toUTCString) { 
+                options.expires = expires.toUTCString();
+            }
+            
+            value = encodeURIComponent(value);
+            
+            var updatedCookie = name + "=" + value;
+            
+            for(var propName in options) {
+                updatedCookie += "; " + propName;
+                var propValue = options[propName];    
+                if (propValue !== true) { 
+                  updatedCookie += "=" + propValue;
+                }
+            }
+            document.cookie = updatedCookie;
+        },
+        eraseCookie: function(name) {
+            obj.cookies.setCookie(name,"",-1)
         }
+    }
     
-    this.showPopup = function () {
-            if (this.cookies.getCookie('cleverPopup') != "") {
-                end = this.cookies.getCookie('cleverPopup')
+    obj.showPopup = function () {
+            console.log('ok');
+            if (obj.cookies.getCookie('module') != "") {
+                end = obj.cookies.getCookie('module');
+                console.log('has cookie')
             } else {
                 var end = 1;
                     flag = true;
-                this.cookies.createCookie('cleverPopup', end, 30);
-                this.condition(function(result){
+                    console.log('cookies have created')
+                obj.cookies.setCookie('module', end, 30);
+                obj.condition(function(result){ 
                     if (result && flag) {
-                        $('a.link-cleverpopup').click(); //jquery plugin fancybox
+                        console.log('it is work!'); 
                        flag = false;
                     }
                 })
             }
     }
-  }  
-popup = new cleverPopup();
-popup.showPopup(); 
-})
+
+    return obj;
+})()
